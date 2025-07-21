8450ff88a57a6e656ed3a6211eeee6c7ef8e9d0f
Luiz Veronesi <luiz.veronesi@outlook.com>
2025-07-21 11:01:26 +0200

    latest

--- conf/mongo-init.js
+++ conf/mongo-init.js
@@ -1,3 +1,10 @@
+// switch to the "tycho" db
+db = db.getSiblingDB('tycho');
+
+// create a dummy collection to force db creation
+db.dummy.insertOne({ init: true });
+
+// now create the user
 db.createUser({
   user: 'tycho',
   pwd: 'tycho_pwd',
---
+++ install/install.bat
@@ -0,0 +1,4 @@
+# create folder /dir/httpd
+# create folder /dir/httpd/upload
+# create folder /dir/httpd/icons
+# add icons
