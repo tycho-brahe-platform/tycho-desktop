726af2d1f8171c7ab5875c6a3c022bac451f695b
Luiz Veronesi <luiz.veronesi@outlook.com>
2025-07-28 17:47:20 +0200

    fix scripts restore/backup

--- scripts/restore-data-mongo.sh
+++ scripts/restore-data-mongo.sh
@@ -10,17 +10,22 @@ inputDir="${localDir}input/"
 tempDir="${inputDir}temp/"
 filesDir="${localDir}files/"
 
+# Create necessary folders if they do not exist
+mkdir -p "$tempDir"
+mkdir -p "$filesDir"
+
 # Clean previous temp folder if exists
 rm -R "${tempDir}${corpus}" 2>/dev/null
 
 # Extract the tar file into the temp directory
 tar -zxvf "${filename}" -C "${tempDir}"
 
+# Import each JSON file into the MongoDB container
 for file in "${tempDir}${corpus}"/*.json; do
 	collection=$(basename "$file" .json)
-	docker exec -t ${container} mongoimport --db ${database} --collection "$collection" --file "$file" --jsonArray --mode=upsert --upsertFields _id
+	docker exec -t "${container}" mongoimport --db "${database}" --collection "$collection" --file "$file" --jsonArray --mode=upsert --upsertFields _id
 done
 
 # Clean up
-rm -R "${tempDir}/${corpus}"
+rm -R "${tempDir}${corpus}"
 mv "${filename}" "${filesDir}"
